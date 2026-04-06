1f629df632b271a586253d7941a081806e54658b
Thierno <thiernoabalde5@gmail.com>
2026-04-06 08:28:41 -0400

    Lab 2: Variables, outputs and reusable code

--- main.tf
+++ main.tf
@@ -1,20 +1,21 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_instance" "web" {
-  ami           = "ami-0c02fb55956c7d316"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "TerraformLab"
-  }
-}
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "= 5.31.0"
     }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = var.instance_type
+
+  tags = {
+    Name = var.instance_name
   }
 }
